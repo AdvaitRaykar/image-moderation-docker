be5708a3f763fb77a99a6319dc8784a449bc9d3c
Advait Raykar <t-advait.raykar@traveloka.com>
2020-01-23 10:58:24 +0530

    adds api endpoint to post image directly

--- classify_nsfw.py
+++ classify_nsfw.py
@@ -116,6 +116,30 @@ def main(argv):
     get_score(args.input_file, args.model_def, args.pretrained_model)
 
 
+def get_score_from_image(file, model=default_model, pretrained_model=default_pretrained_model):
+    image_data = file
+
+    # Pre-load caffe model.
+    nsfw_net = caffe.Net(model,  # pylint: disable=invalid-name
+                         pretrained_model, caffe.TEST)
+
+    # Load transformer
+    # Note that the parameters are hard-coded for best results
+    caffe_transformer = caffe.io.Transformer({'data': nsfw_net.blobs['data'].data.shape})
+    caffe_transformer.set_transpose('data', (2, 0, 1))  # move image channels to outermost
+    caffe_transformer.set_mean('data', np.array([104, 117, 123]))  # subtract the dataset-mean value in each channel
+    caffe_transformer.set_raw_scale('data', 255)  # rescale from [0, 1] to [0, 255]
+    caffe_transformer.set_channel_swap('data', (2, 1, 0))  # swap channels from RGB to BGR
+
+    # Classify.
+    scores = caffe_preprocess_and_compute(image_data, caffe_transformer=caffe_transformer, caffe_net=nsfw_net,
+                                          output_layers=['prob'])
+
+    # Scores is the array containing SFW / NSFW image probabilities
+    # scores[1] indicates the NSFW probability
+    print "NSFW score:  ", scores[1]
+    return scores[1]
+
 def get_score(file, model=default_model, pretrained_model=default_pretrained_model):
     tmp_filename = ""
     if file.find('http') != -1:
@@ -131,6 +155,7 @@ def get_score(file, model=default_model, pretrained_model=default_pretrained_mod
         file = tmp_filename
 
     image_data = open(file).read()
+    print(type(image_data))
 
     # Pre-load caffe model.
     nsfw_net = caffe.Net(model,  # pylint: disable=invalid-name
--- server.py
+++ server.py
@@ -5,7 +5,11 @@ import logging
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import classify_nsfw
 from flask import Flask
-
+from flask import request
+import StringIO
+from PIL import Image
+import base64
+import cStringIO
  
 app = Flask(__name__) 
   
@@ -23,8 +27,14 @@ def parse_from_url(url):
 
 @app.route('/image', methods=['POST'])
 def parse_from_image():
-    image = request.files['file']
-    
+    image = request.files['file'].stream
+    buffer = cStringIO.StringIO()
+    # image.save(buffer, format="JPEG")
+    # img_str = base64.b64encode(buffer.getvalue())
+    message = classify_nsfw.get_score_from_image(image.read())
+    # classify_nsfw.resize_image(image.read())
+    # message = 'test'
+    return str(message)    
 
 if __name__ == '__main__':  
     app.run(host='0.0.0.0')
